b207872e4e17f7c766e36f4af5c0ad68fa393001
Jair Oliveira <jair.almeida.oliveira@gmail.com>
2023-05-30 18:17:42 -0100

    refactor linted

--- src/sever.d.ts
+++ src/sever.d.ts
@@ -1,7 +1,8 @@
-import { OAuth2Namespace } from "@fastify/oauth2";
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+import {type OAuth2Namespace} from '@fastify/oauth2';
 
 declare module 'fastify' {
-  interface FastifyInstance {
-    googleOAuth2: OAuth2Namespace;
-  }
+	interface FastifyInstance {
+		googleOAuth2: OAuth2Namespace;
+	}
 }
