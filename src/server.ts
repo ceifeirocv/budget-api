294a3b82191098624bbb755625b37e2d95a0b91d
Jair Oliveira <jair.almeida.oliveira@gmail.com>
2023-05-30 17:51:06 -0100

    add fastify/oauth2

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,37 @@
 import fastify from 'fastify'
+import oauthPlugin from "@fastify/oauth2";
 
 const server = fastify()
+
+server.register(oauthPlugin, {
+  name: 'googleOAuth2',
+  scope: ['profile', 'email'],
+  credentials: {
+    client: {
+      id: '1048568934801-s9b0i2ubpktif27k7g5de50591244irk.apps.googleusercontent.com',
+      secret: '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'
+    },
+    auth: oauthPlugin.GOOGLE_CONFIGURATION
+  },
+  // register a fastify url to start the redirect flow
+  startRedirectPath: '/login/google',
+  // facebook redirect here after the user login
+  callbackUri: 'http://localhost:4000/auth/google/callback'
+})
+
+server.get('/auth/google/callback', async function (request, reply) {
+  const { token } = await this.googleOAuth2.getAccessTokenFromAuthorizationCodeFlow(request)
+  console.log(token)
+
+  return token
+  
+})
 
 server.get('/ping', async (request, reply) => {
   return 'pong\n'
 })
 
-server.listen({ port: 8080 }, (err, address) => {
+server.listen({ port: 4000 }, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
