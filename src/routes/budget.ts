998340a04da6626ede80198f1382b0face73ddb6
Jair Oliveira <jair.almeida.oliveira@gmail.com>
2023-06-08 21:36:17 -0100

    update budget routes

--- src/routes/budget.ts
+++ src/routes/budget.ts
@@ -24,7 +24,67 @@ export async function budgetRoute(fastify: FastifyInstance) {
 				},
 			});
 
-			return budget;
+			return {budget};
+		} catch (error) {
+			return error;
+		}
+	});
+
+	fastify.get('/budget', async (request, reply) => {
+		try {
+			const budgets = await prisma.budget.findMany({
+				where: {
+					userId: request.user.sub,
+				},
+			});
+			return {budgets};
+		} catch (error) {
+			return error;
+		}
+	});
+
+	fastify.get('/budget/:budgetId', async (request, reply) => {
+		const paramsSchema = z.object({
+			budgetId: z.string().uuid(),
+		});
+
+		try {
+			const {budgetId} = paramsSchema.parse(request.params);
+			const budget = await prisma.budget.findFirst({
+				where: {
+					userId: request.user.sub,
+					id: budgetId,
+				},
+			});
+			return {budget};
+		} catch (error) {
+			return error;
+		}
+	});
+
+	fastify.put('/budget/:budgetId', async (request, reply) => {
+		const paramsSchema = z.object({
+			budgetId: z.string().uuid(),
+		});
+		const bodySchema = z.object({
+			name: z.string(),
+			amount: z.coerce.number(),
+		});
+
+		try {
+			const {budgetId} = paramsSchema.parse(request.params);
+			const {name, amount} = bodySchema.parse(request.body);
+			const budget = await prisma.budget.update({
+				where: {
+					id: budgetId,
+				},
+				data: {
+					name,
+					amount,
+					userId: request.user.sub,
+				},
+			});
+			return {budget};
 		} catch (error) {
 			return error;
 		}
