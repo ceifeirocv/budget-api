fc64f5cf24b688c0a6b58ff42ece79dd3c110a54
Jair Oliveira <jair.almeida.oliveira@gmail.com>
2023-06-08 21:36:35 -0100

    add color to budget schema

--- prisma/migrations/20230601192831_create_budget_schema/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Budget" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT,
-    "amount" REAL NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "userId" TEXT,
-    CONSTRAINT "Budget_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
-);
